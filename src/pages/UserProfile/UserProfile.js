f0f79f9d952309260878f52af43a14c6edb60881
AmanKrs <imkumarak4@gmail.com>
2024-05-05 20:38:15 +0530

    .env removed from tracking

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Navbar from "../../component/Navbar/Navbar";
+
 const apiurl = process.env.REACT_APP_API_URL;
 export default function UserProfile() {
-  return <div>UserProfile</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">UserProfile</div>
+    </>
+  );
 }
